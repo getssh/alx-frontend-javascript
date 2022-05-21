6e64b4e0f9080a73abacb5668f3684d99e199fd3
getssh <gtamrat33@gmail.com>
2022-05-21 14:26:45 +0300

    lint fixed

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,12 +1,12 @@
 export default function hasValuesFromArray(set, arr) {
-    let new_set = new Set(arr);
-    let test = false;
+  let new_set = new Set(arr);
+  let test = false;
 
-    for (let val of set) {
-	for (let val2 of new_set) {
-	    test = set.has(val2);
-	}
+  for (let val of set) {
+    for (let val2 of new_set) {
+      test = set.has(val2);
     }
+  }
 
-    return test;
+  return test;
 }
