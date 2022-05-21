99c3eb7bc128dd4fcc6c6a8c9447874e2e57d8d3
getssh <gtamrat33@gmail.com>
2022-05-21 14:12:07 +0300

    another way

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,12 @@
 export default function hasValuesFromArray(set, arr) {
-  return arr.reduce((prev, curr) => prev && set.has(curr), true);
+    let new_set = new Set(arr);
+    let test = false;
+
+    for (let val of set) {
+	for (let val2 of new_set) {
+	    test = set.has(val2);
+	}
+    }
+
+    return test;
 }
